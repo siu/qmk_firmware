fc90251454aad6acb878f3704e4f971ae2e1bced
David Siñuela Pastor <siu.4coders@gmail.com>
2021-11-29 16:25:21 +0100

    Add custom yaemk keymap

---
+++ keyboards/karlk90/yaemk/keymaps/siu/rules.mk
@@ -0,0 +1,7 @@
+# QMK features
+#CAPS_WORD_ENABLE                   = yes
+#EXTRAKEY_ENABLE                    = yes
+#MOUSEKEY_ENABLE                    = yes
+#NKRO_ENABLE                        = yes
+OLED_ENABLE                        = no
+RGB_MATRIX_ENABLE                  = no
