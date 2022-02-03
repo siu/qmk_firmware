c5873686e40bb8b4f26ccffc8ec5d79fa973537f
David Siñuela Pastor <siu.4coders@gmail.com>
2022-02-03 23:42:14 +0100

    Make brackets and -= more accessible

--- keyboards/karlk90/yaemk/keymaps/siu/keymap.c
+++ keyboards/karlk90/yaemk/keymaps/siu/keymap.c
@@ -61,15 +61,15 @@ enum Encoder {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_BASE] = LAYOUT( \
      KC_GRV         , KC_1          , KC_2          , KC_3          , KC_4          , KC_5          , KC_NO         ,                                 KC_NO         , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_CAPS       ,\
-     KC_TAB         , KC_Q          , KC_W          , KC_E          , KC_R          , KC_T          , KC_NO         ,                                 KC_NO         , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLS       ,\
-     KC_ESC         , KC_A          , KC_S          , KC_D          , KC_F          , KC_G          , KC_NO         ,                                 KC_NO         , KC_H          , KC_J          , KC_K          , KC_L          , KC_SCLN       , KC_QUOT       ,\
+     KC_TAB         , KC_Q          , KC_W          , KC_E          , KC_R          , KC_T          , KC_LBRC       ,                                 KC_RBRC       , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLS       ,\
+     KC_ESC         , KC_A          , KC_S          , KC_D          , KC_F          , KC_G          , KC_MINS       ,                                 KC_EQL        , KC_H          , KC_J          , KC_K          , KC_L          , KC_SCLN       , KC_QUOT       ,\
      KC_LSPO        , KC_Z          , KC_X          , KC_C          , KC_V          , KC_B          , _______       , TG(_ADJUST)   , TG(_ADJUST)   , _______       , KC_N          , KC_M          , KC_COMM       , KC_DOT        , KC_SLSH       , KC_RSPC       ,\
                                                       BASE          , KC_LGUI       , KC_LCTRL      , KC_SPC        , KC_TAB        , KC_BSPC       , KC_ENT        , MO(_LOWER)    , MO(_RAISE)    , BASE2 \
   ),
    [_BASE2] = LAYOUT( \
      KC_GRV         , KC_1          , KC_2          , KC_3          , KC_4          , KC_5          , KC_NO         ,                                 KC_NO         , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_CAPS       ,\
-     KC_TAB         , KC_Q          , KC_W          , KC_E          , KC_R          , KC_T          , KC_NO         ,                                 KC_NO         , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLS       ,\
-     KC_ESC         , KC_A          , KC_S          , KC_D          , KC_F          , KC_G          , KC_NO         ,                                 KC_NO         , KC_H          , KC_J          , KC_K          , KC_L          , KC_SCLN       , KC_QUOT       ,\
+     KC_TAB         , KC_Q          , KC_W          , KC_E          , KC_R          , KC_T          , KC_LBRC       ,                                 KC_RBRC       , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_BSLS       ,\
+     KC_ESC         , KC_A          , KC_S          , KC_D          , KC_F          , KC_G          , KC_MINS       ,                                 KC_EQL        , KC_H          , KC_J          , KC_K          , KC_L          , KC_SCLN       , KC_QUOT       ,\
      KC_LSPO        , KC_Z          , KC_X          , KC_C          , KC_V          , KC_B          , _______       , TG(_ADJUST)   , TG(_ADJUST)   , _______       , KC_N          , KC_M          , KC_COMM       , KC_DOT        , KC_SLSH       , KC_RSPC       ,\
                                                       BASE          , KC_LGUI       , MO(_LOWER)    , KC_SPC        , CTL_TAB       , KC_BSPC       , KC_ENT        , MO(_RAISE)    , KC_RALT       , BASE2 \
   ),
