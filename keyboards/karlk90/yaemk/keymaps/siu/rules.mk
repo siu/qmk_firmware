76c5ce88338f77df91e6d1f3ab07c0baa923393c
David Siñuela Pastor <siu.4coders@gmail.com>
2022-12-04 22:56:02 +0100

    Disable rgb matrix to fix compilation

---
+++ keyboards/karlk90/yaemk/keymaps/siu/rules.mk
@@ -0,0 +1,7 @@
+# QMK features
+#CAPS_WORD_ENABLE                   = yes
+#EXTRAKEY_ENABLE                    = yes
+#MOUSEKEY_ENABLE                    = yes
+#NKRO_ENABLE                        = yes
+#OLED_ENABLE                        = yes
+RGB_MATRIX_ENABLE                  = no
